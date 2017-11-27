77468cfd61c35e2a3a5e2ffb17a6900d3d46a9e2
Kenneth Blix <kennethblix@gmail.com>
2017-11-27 10:43:54 +0100

    Library for Vendor, Item, Store

---
+++ ConsignmentShop/ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsignmentShopLibrary
+{
+    public class Vendor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Commission { get; set; }
+
+
+    }
+}
